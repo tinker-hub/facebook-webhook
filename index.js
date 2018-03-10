abbf1146465127696a3032a4b85e4e958009a763
jmaicaaan <jmsantosss29@yahoo.com>
2018-03-10 11:23:03 +0800

    Facebook bot

--- index.js
+++ index.js
@@ -9,28 +9,27 @@ const chrono = require('chrono-node')
 var schedule = require('node-schedule')
 const EventEmitter = require('events').EventEmitter
 
-var config = {}
-
-const reminders = []
+const config = {};
+const reminders = [];
 
 const eventEmitter = new EventEmitter()
 
 app.set('port', (process.env.PORT || 5000))
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-app.get('/', function(req, res) {
-  res.send("hey there boi")
+app.get('/', function (req, res) {
+  res.redirect('https://www.facebook.com/Na-Santos-Dre-191598887555824/');
 })
 
-app.get('/webhook/', function(req, res) {
-  if (req.query['hub.verify_token'] === process.env.VERIFY_TOKEN){
+app.get('/webhook/', function (req, res) {
+  if (req.query['hub.verify_token'] === process.env.VERIFY_TOKEN) {
     return res.send(req.query['hub.challenge'])
   }
   res.send('wrong token')
 })
 
-app.listen(app.get('port'), function(){
+app.listen(app.get('port'), function () {
   console.log('Started on port', app.get('port'))
 })
 
@@ -41,36 +40,37 @@ const bot = new BootBot({
   appSecret: process.env.APP_SECRET
 })
 
-bot.setGreetingText("Hello, I'm here to help you manage your tasks. Be sure to setup your bucket by typing 'Setup'. ")
+bot.setGreetingText("Hello there, I'm here to help you manage your tasks. Be sure to setup your bucket by typing 'Setup'. ")
 
 bot.setGetStartedButton((payload, chat) => {
-  if(config.bucket === undefined){
-    chat.say('Hello my name is Note Buddy and I can help you keep track of your thoughts')
+  if (!config.bucket) {
+    chat.say('Hello my name is Kitty and I can help you keep track of your thoughts')
     chat.say("It seems like you have not setup your bucket settings yet. That has to be done before you can do anything else. Make sure to type 'setup'")
   }
   BotUserId = payload.sender.id
 });
 
 bot.hear('setup', (payload, chat) => {
+  console.log(payload);
   const getBucketSlug = (convo) => {
     convo.ask("What's your Bucket's slug?", (payload, convo) => {
       var slug = payload.message.text;
       convo.set('slug', slug)
-      convo.say("setting slug as "+slug).then(() => getBucketReadKey(convo));
+      convo.say("setting slug as " + slug).then(() => getBucketReadKey(convo));
     })
   }
   const getBucketReadKey = (convo) => {
     convo.ask("What's your Bucket's read key?", (payload, convo) => {
       var readkey = payload.message.text;
       convo.set('read_key', readkey)
-      convo.say('setting read_key as '+readkey).then(() => getBucketWriteKey(convo))
+      convo.say('setting read_key as ' + readkey).then(() => getBucketWriteKey(convo))
     })
   }
   const getBucketWriteKey = (convo) => {
     convo.ask("What's your Bucket's write key?", (payload, convo) => {
       var writekey = payload.message.text
       convo.set('write_key', writekey)
-      convo.say('setting write_key as '+writekey).then(() => finishing(convo))
+      convo.say('setting write_key as ' + writekey).then(() => finishing(convo))
     })
   }
   const finishing = (convo) => {
@@ -83,24 +83,25 @@ bot.hear('setup', (payload, chat) => {
     convo.say('All set :)')
     convo.end();
   }
-  
+
   chat.conversation((convo) => {
     getBucketSlug(convo)
   })
 })
 
-bot.hear(['hello', 'hey', 'sup'], (payload, chat)=>{
+bot.hear(['hello', 'hey', 'sup'], (payload, chat) => {
   chat.getUserProfile().then((user) => {
     chat.say(`Hey ${user.first_name}, How are you today?`)
   })
 })
 
 bot.hear('config', (payload, chat) => {
-  if(JSON.stringify(config.bucket) === undefined){
+  if (!config.bucket) {
     chat.say("No config found :/ Be sure to run 'setup' to add your bucket details")
+  } else {
+    chat.say("A config has been found :) " + JSON.stringify(config.bucket))
   }
-  chat.say("A config has been found :) "+ JSON.stringify(config.bucket))
-})
+});
 
 bot.hear('create', (payload, chat) => {
   chat.conversation((convo) => {
@@ -111,15 +112,15 @@ bot.hear('create', (payload, chat) => {
         type_slug: 'reminders',
         title: payload.message.text,
         metafields: [
-         {
-           key: 'date',
-           type: 'text',
-           value: datetime
-         }
+          {
+            key: 'date',
+            type: 'text',
+            value: datetime
+          }
         ]
       }
-      Cosmic.addObject(config, params, function(error, response){
-        if(!error){
+      Cosmic.addObject(config, params, function (error, response) {
+        if (!error) {
           eventEmitter.emit('new', response.object.slug, datetime)
           convo.say("reminder added correctly :)")
           convo.end()
@@ -139,15 +140,15 @@ bot.hear('help', (payload, chat) => {
   chat.say("'config': lists your current bucket config")
 })
 
-eventEmitter.on('new', function(itemSlug, time) {
-  schedule.scheduleJob(time, function(){
-    Cosmic.getObject(config, {slug: itemSlug}, function(error, response){
-      if(response.object.metadata.date == new Date(time).toISOString()){
+eventEmitter.on('new', function (itemSlug, time) {
+  schedule.scheduleJob(time, function () {
+    Cosmic.getObject(config, { slug: itemSlug }, function (error, response) {
+      if (response.object.metadata.date == new Date(time).toISOString()) {
         bot.say(BotUserId, response.object.title)
         console.log('firing reminder')
       } else {
         eventEmitter.emit('new', response.object.slug, response.object.metafield.date.value)
-        console.log('times do not match checking again at '+response.object.metadata.date)
+        console.log('times do not match checking again at ' + response.object.metadata.date)
       }
     })
   })
